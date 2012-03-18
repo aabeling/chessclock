b93dc3a589c6f38beba52b6239148b0feb544ac3
Achim Abeling <achimabeling@web.de>
2012-03-18 11:49:41 +0100

    Incremented version to 0.4. Screen shutoff is turned off now.

--- src/de/banapple/android/chessclock/App.java
+++ src/de/banapple/android/chessclock/App.java
@@ -3,6 +3,7 @@ package de.banapple.android.chessclock;
 import org.apache.cordova.DroidGap;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class App extends DroidGap 
 {
@@ -11,5 +12,6 @@ public class App extends DroidGap
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
